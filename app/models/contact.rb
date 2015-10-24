5bab092fda4aea518f2972bc7578bdcdf7f8382f
CC Evans <ccevans1@gmail.com>
2015-10-24 15:33:31 -0400

    updating mobile css

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,10 @@
 class Contact < MailForm::Base
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :message,    :validate => true
+  attribute :phone,     :valideate => /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/
+     
   attribute :nickname,  :captcha  => true
-  attribute :status 
+  attribute :time 
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
@@ -15,6 +16,6 @@ class Contact < MailForm::Base
     }
   end
 
-  STATUS = %w[$1k-5k $5k-10k $10k-20k $20k-50k]
+  BESTTIME = %w[Mornings Afternoons Evenings]
 
 end
