4fb385d068dff5fe4b9a7c418ea5a940034846f7
CC Evans <ccevans1@gmail.com>
2015-10-27 11:08:58 -0400

    go live

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # get 'pages/home'
   
-  get "pages/home"
-  get "pages/newhome"
+  # get "pages/home"
+ # get "pages#launch"
 
-  root "pages#launch"
+  root "pages#newhome"
 
   match '/contacts', to: 'contacts#new', via: 'get'
 resources "contacts", only: [:new, :create]
